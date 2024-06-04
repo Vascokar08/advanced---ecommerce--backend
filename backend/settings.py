0b55f077402513b8ac04d9b32000b541e55e6fd4
SHIVAM KORGAONKAR <115925079+Vascokar08@users.noreply.github.com>
2024-06-04 20:43:06 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -23,9 +23,8 @@ SECRET_KEY = 'django-insecure-b*tuoe%^o+=^35$0fufrm=oamh^(o0tabn39(7ni12(i-oup+4
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ["website-domain.com", "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = ['https://website-domain.com', 'https://127.0.0.1']
+ALLOWED_HOSTS = ["multivendor-ecommerce-api.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ['https://multivendor-ecommerce-api.up.railway.app']
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
 
 
